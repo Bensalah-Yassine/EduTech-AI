03fd0acba2213af92ccbc03f072918554857ab12
YASSINE <bensalah.yassine.tech@gmail.com>
2025-06-25 00:37:26 +0100

    implement HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CompanionCard from '@/components/CompanionCard';
 import CompanionList from '@/components/CompanionList';
 import CTA from '@/components/CTA';
+import { recentSessions } from '@/constants';
 
 const Page = () => {
   return (
@@ -38,7 +39,11 @@ const Page = () => {
         />
       </section>
       <section className='home-section'>
-        <CompanionList />
+        <CompanionList 
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
         <CTA />
 
       </section>
