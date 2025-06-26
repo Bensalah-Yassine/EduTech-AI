6f2ecfcc16a3da7bd11f937956725c9758d663da
YASSINE <bensalah.yassine.tech@gmail.com>
2025-06-26 06:28:41 +0100

    Conversations & Session History

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,29 @@
 import CompanionCard from '@/components/CompanionCard';
 import CompanionList from '@/components/CompanionList';
 import CTA from '@/components/CTA';
-import { recentSessions } from '@/constants';
+import { recentSessions, subjects } from '@/constants';
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit : 3});
+  const recentSessions = await getRecentSessions({ limit : 10});
   return (
     <main>
       <h1>
         Popular Companions
       </h1>
       <section className='home-section'>
-        <CompanionCard 
-          id ="123"
-          name="Neura the brainy explorer"
-          topic="Neural Network of The Brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            color={getSubjectColor(companion.subject)}
+            {...companion}
+          
+          />
 
-        />
-        <CompanionCard 
-          id ="456"
-          name="Neura the brainy explorer"
-          topic="Neural Network of The Brain"
-          subject="science"
-          duration={45}
-          color="#e5d0ff"
-
-        />
-        <CompanionCard 
-          id ="789"
-          name="Neura the brainy explorer"
-          topic="Neural Network of The Brain"
-          subject="science"
-          duration={45}
-          color="#bde7ff"
-
-        />
+        ))}
+        
       </section>
       <section className='home-section'>
         <CompanionList 
