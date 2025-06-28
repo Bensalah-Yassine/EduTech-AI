d885a3520847c3f159a8f0adee3c896b188a732c
YASSINE <bensalah.yassine.tech@gmail.com>
2025-06-28 23:48:42 +0100

    Fix Some Errors

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -176,11 +176,11 @@ export const getBookmarkedCompanions = async (userId: string) => {
   const supabase = createSupabaseClient();
   const { data, error } = await supabase
     .from("bookmarks")
-    .select(`companions:companion_id (*)`) // Notice the (*) to get all the companion data
+    .select(`companions:companion_id(*)`) // Notice the (*) to get all the companion data
     .eq("user_id", userId);
-  if (error) {
-    throw new Error(error.message);
-  }
+  //if (error) {
+  //  throw new Error(error.message);
+  //}
   // We don't need the bookmarks data, so we return only the companions
-  return data.map(({ companions }) => companions);
+  //return data.map(({ companions }) => companions);
 };
